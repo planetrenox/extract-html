710d0378bb156955e5243102793a38508fb67542
renox <planetrenox@protonmail.com>
2024-03-09 04:29:43 -0800

    i

--- 2nd.js
+++ 2nd.js
@@ -1,5 +1,4 @@
-function downloadPageContent()
-{
+function downloadPageContent() {
     const clone = document.documentElement.cloneNode(true);
 
     // Remove scripts
@@ -10,10 +9,8 @@ function downloadPageContent()
 
     // Remove comments
     const comments = clone.querySelectorAll('*');
-    comments.forEach(node =>
-    {
-        node.childNodes.forEach(child =>
-        {
+    comments.forEach(node => {
+        node.childNodes.forEach(child => {
             if (child.nodeType === Node.COMMENT_NODE) {
                 node.removeChild(child);
             }
@@ -24,20 +21,14 @@ function downloadPageContent()
     const hiddenElements = clone.querySelectorAll('[hidden], [style*="display:none"], [style*="display: none"]');
     hiddenElements.forEach(el => el.remove());
 
-    // // Remove empty elements
-    // const emptyElements = clone.querySelectorAll(':empty');
-    // emptyElements.forEach(el => el.remove());
-
     // Remove non-visible elements
     const nonVisibleElements = clone.querySelectorAll('head, meta, link, style, script, noscript, title, base, param, source, track');
     nonVisibleElements.forEach(el => el.remove());
 
     // Remove data attributes
     const allElements = clone.querySelectorAll('*');
-    allElements.forEach(el =>
-    {
-        Array.from(el.attributes).forEach(attr =>
-        {
+    allElements.forEach(el => {
+        Array.from(el.attributes).forEach(attr => {
             if (attr.name.startsWith('data-')) {
                 el.removeAttribute(attr.name);
             }
@@ -45,10 +36,8 @@ function downloadPageContent()
     });
 
     // Remove inline event handlers
-    allElements.forEach(el =>
-    {
-        Array.from(el.attributes).forEach(attr =>
-        {
+    allElements.forEach(el => {
+        Array.from(el.attributes).forEach(attr => {
             if (attr.name.startsWith('on')) {
                 el.removeAttribute(attr.name);
             }
@@ -56,18 +45,20 @@ function downloadPageContent()
     });
 
     const html = clone.outerHTML;
-    const css = Array.from(document.styleSheets).map(sheet =>
-    {
+    const css = Array.from(document.styleSheets)
+    .map(sheet => {
         try {
-            return Array.from(sheet.cssRules).map(rule => rule.cssText).join('');
-        }
-        catch (e) {
+            return Array.from(sheet.cssRules)
+            .map(rule => rule.cssText)
+            .join('');
+        } catch (e) {
             console.warn('Error accessing cssRules for stylesheet:', sheet.href);
             return '';
         }
-    }).join('');
+    })
+    .join('');
 
-    const blob = new Blob([html, '<style>', css, '</style>'], {type: 'text/html'});
+    const blob = new Blob([html, '<style>', css, '</style>'], { type: 'text/html' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
